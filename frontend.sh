a89fcead8a03ff864caf82d571ead24b44dcc05a
Juno <ameet.hosur999@gmail.com>
2023-05-28 19:07:34 -0400

    frontend script

--- frontend.sh
+++ frontend.sh
@@ -16,5 +16,5 @@ echo -e "\e[33mUpdate Frontend Configuration\e[0m"
 cp /home/centos/roboshop-shell/roboshop.conf /etc/nginx/default.d/roboshop.conf &>>/tmp/roboshop.log
 
 echo -e "\e[33mStarting Nginx Server\e[0m"
-systemctl enable nginx
-systemctl restart nginx
+systemctl enable nginx &>>/tmp/roboshop.log
+systemctl restart nginx &>>/tmp/roboshop.log
